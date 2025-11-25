f88d8361dba983683b83e14121948759ba38e6c0
reza0002 <remo0002@stud.ek.dk>
2025-11-25 10:11:15 +0100

    Test3

--- src/SmashTennisClub/MainPackage/EnumLists/DisciplineType.java
+++ src/SmashTennisClub/MainPackage/EnumLists/DisciplineType.java
@@ -1,4 +1,28 @@
 package SmashTennisClub.MainPackage.EnumLists;
 
 public enum DisciplineType {
+    JUNIOR(800.00),
+    SENIOR(1500.00),
+    //    PENSIONIST(1125.00),
+    PASSIVT(250.00);
+
+
+    private final double price;
+
+    DisciplineType(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Payment price: " + price + " kr";
+    }
 }
+
+
+
