f6b8e794a890986b3122cc1e4de060ecdd346b56
reza0002 <remo0002@stud.ek.dk>
2025-11-27 13:25:26 +0100

    Test14

--- src/SmashTennisClub/MainPackage/Main.java
+++ src/SmashTennisClub/MainPackage/Main.java
@@ -3,8 +3,10 @@ package SmashTennisClub.MainPackage;
 import SmashTennisClub.FileSystem.FileHandler;
 import SmashTennisClub.MainPackage.EnumLists.Gender;
 import SmashTennisClub.MainPackage.EnumLists.MembershipPricelist;
+import SmashTennisClub.MainPackage.FinanceManagement.Quota;
 import SmashTennisClub.MainPackage.MembershipTypes.Member;
 import SmashTennisClub.MainPackage.UserPackage.Chairman;
+import SmashTennisClub.MainPackage.UserPackage.Treasurer;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,12 +15,16 @@ public class Main {
     public static void main(String[] args) {
         Chairman chairman = new Chairman();
         FileHandler fileHandler = new FileHandler();
-//        ArrayList<Member> Members = new ArrayList<Member>();
+//        Treasurer treasurer = new Treasurer();
+//        ArrayList<Quota> embers = new ArrayList<Quota>();
+//        embers.add(new Quota(10, 15,"lars", 1500.00,true,
+//                2025,03, 05, 2025,06,06"));
 
 
        // chairman.createMemberTest();
-        fileHandler.printAllMembers();
-        chairman.deleteMember();
+//        fileHandler.printAllMembers();
+//        chairman.deleteMember();
+//        treasurer.searchForQuota();
 
 
     }
