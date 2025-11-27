e7acf6e66da8bf0fcfce870daf833086f54a69b1
Muhammedsarac-debug <muhammed.sarac@outlook.dk>
2025-11-27 11:01:56 +0100

    Har tilføjet getters og en toString i TrainingSession

--- src/SmashTennisClub/MainPackage/PlayerStatistic/TrainingSession.java
+++ src/SmashTennisClub/MainPackage/PlayerStatistic/TrainingSession.java
@@ -12,7 +12,6 @@ public class TrainingSession {
     Date dateOfTraining;
     int memberId; //(for alle spillere)
     String memberName;
-
     int amountOfWonSets;
     MatchResult trainingResult;
 
@@ -27,5 +26,20 @@ public class TrainingSession {
         this.memberName = memberName;
         this.amountOfWonSets = amountOfWonSets;
         this.trainingResult = trainingResult;
+    }
+
+    public int getTrainingId() { return trainingId; }
+    public String getMemberName() { return memberName; }
+    public int getMemberId() { return memberId; }
+    public DisciplineType getDisciplineTypeForTraining() { return disciplineTypeForTraining; }
+    public Date getDateOfTraining() { return dateOfTraining; }
+    public int getAmountOfWonSets() { return amountOfWonSets; }
+    public MatchResult getTrainingResult() { return trainingResult; }
+
+    @Override
+    public String toString() {
+        return "TrainingSession {" + "trainingId=" + trainingId + ", member=" + memberName + " (" + memberId + ")"
+                + ", discipline=" + disciplineTypeForTraining + ", date=" + dateOfTraining + ", won sets=" + amountOfWonSets +
+                ", result=" + trainingResult + "}";
     }
 }
