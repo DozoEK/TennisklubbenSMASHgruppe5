dc6accc3a431787cd3d6c0c851d345ea6f18362f
DozoEK <lask0005@stud.ek.dk>
2025-11-28 13:27:44 +0100

    menu beta 0.2

--- src/SmashTennisClub/MainPackage/UserPackage/menu/menu.java
+++ src/SmashTennisClub/MainPackage/UserPackage/menu/menu.java
@@ -1,7 +1,12 @@
 package SmashTennisClub.MainPackage.UserPackage.menu;
+import SmashTennisClub.MainPackage.UserPackage.Chairman;
+import SmashTennisClub.MainPackage.UserPackage.Treasurer;
+
 import java.util.Scanner;
 
 public class menu {
+    Chairman chairman = new Chairman();
+    Treasurer treasurer = new Treasurer();
     private boolean running;
     Scanner scanner = new Scanner(System.in);
 
@@ -28,7 +33,7 @@ public class menu {
         System.out.println("1. Søg efter medlem");
         System.out.println("2. Slet et medlem");
         System.out.println("3. Vis betalte kontigenter ");
-        System.out.println("4. Vis ");
+//        System.out.println("4. Vis ");
 //        System.out.println("5. Vis Produkter ");
 //        System.out.println("6. vis Budget (kræver password)");
         System.out.println("0. Afslut program");
@@ -49,9 +54,9 @@ public class menu {
     //kalder den relevante metode ud fra brugerinput
     private void menuChoise(int choise) {
         switch (choise) {
-            case 1 -> searchForMember();
-            case 2 -> deleteMember();
-            case 3 -> ();
+            case 1 -> chairman.searchForMember();
+            case 2 -> chairman.deleteMember();
+            case 3 -> treasurer.showAllPaidQuotas();
 //            case 4 -> ;
 //            case 5 -> ;
 //            case 6 -> ;
