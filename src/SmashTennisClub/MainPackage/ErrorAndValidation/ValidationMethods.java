548026dd565b6ae8167631281a56ba806ca1dc2f
reza0002 <remo0002@stud.ek.dk>
2025-12-02 09:19:27 +0100

    Test16

---
+++ src/SmashTennisClub/MainPackage/ErrorAndValidation/ValidationMethods.java
@@ -0,0 +1,36 @@
+package SmashTennisClub.MainPackage.ErrorAndValidation;
+
+import SmashTennisClub.FileSystem.SmashException;
+
+import java.time.LocalDate;
+
+public class ValidationMethods implements ValidationInterface {
+
+    @Override
+    public void validateName(String name) throws SmashException {
+        if (name.isEmpty()){
+         throw new SmashException("navnet må ikke være tomt.");
+        }
+        if (!name.matches("[a-z A-ZæøåÆØÅ ]")){
+            throw new SmashException("Navnet må kun indeholde bogstaver!");
+        }
+    }
+
+
+
+    @Override
+    public LocalDate validateDate(String input) {
+        return null;
+    }
+
+    @Override
+    public int validatePhone(String input) {
+        return 0;
+    }
+
+    @Override
+    public boolean validateBoolean(String input) {
+        return false;
+    }
+
+}
